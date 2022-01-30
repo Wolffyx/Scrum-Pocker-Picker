dc3bf09374f5398bc0fb445ad28e0a08f0cec013
Gabriel Wolff <wolffdev@gmail.com>
2022-01-30 12:38:20 +0200

    Github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,16 @@
 export const environment = {
-  firebase: {
-    projectId: 'scrumpockerpicker-255a4',
-    appId: '1:708832724230:web:00a6615bcb91fea015ca87',
-    databaseURL: 'https://scrumpockerpicker-255a4-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'scrumpockerpicker-255a4.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyDmn1Lc53eEK7IuVAZYA9rtSN5Q7RZ7Adc',
-    authDomain: 'scrumpockerpicker-255a4.firebaseapp.com',
-    messagingSenderId: '708832724230',
-    measurementId: 'G-N5RNJMKNLM',
-  },
+	firebase: {
+		locationId: 'europe-west',
+		apiKey: 'AIzaSyDmn1Lc53eEK7IuVAZYA9rtSN5Q7RZ7Adc',
+		authDomain: 'scrumpockerpicker-255a4.firebaseapp.com',
+		databaseURL: 'https://scrumpockerpicker-255a4-default-rtdb.europe-west1.firebasedatabase.app',
+		projectId: 'scrumpockerpicker-255a4',
+		storageBucket: 'scrumpockerpicker-255a4.appspot.com',
+		messagingSenderId: '708832724230',
+		appId: '1:708832724230:web:00a6615bcb91fea015ca87',
+		measurementId: 'G-N5RNJMKNLM',
+	},
+
+	key: 'scrumpockerpicker',
 	production: true,
 };
